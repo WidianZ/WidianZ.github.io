0f83ddf63fa4bf692d350caa7446edffd56673c5
EvEGeNzZz <evegenz@gmail.com>
2017-08-17 16:27:58 +0300

    fix3

--- js/main.js
+++ js/main.js
@@ -24,11 +24,34 @@ $( document ).ready(function() {
 
   $('#email-field').blur(function() {
     var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-    if (testEmail.test(this.value) || $(this).val().length === 0) {
-      $(this).parent().removeClass('has-error');
+    if ($(this).val().length === 0) {
+
+    } else if (testEmail.test(this.value)) {
+      $(this).parent().addClass('has-success');
     } else {
       $(this).parent().addClass('has-error');
     }
   });
 
+  $('#name-field').blur(function() {
+    var testName = /^[a-zA-Zа-яА-Я]+$/;
+    if ($(this).val().length === 0) {
+
+    } else if (testName.test(this.value)) {
+      $(this).parent().addClass('has-success');
+    } else {
+      $(this).parent().addClass('has-error');
+    }
+  });
+
+  $('#name-field, #email-field').focus(function() {
+    $(this).parent().removeClass('has-success');
+    $(this).parent().removeClass('has-error');
+  });
+
+  $('.banner-form_accept').click(function() {
+    if ($(this).siblings('.form-group').hasClass('has-error') || !$('#terms-checkbox').is(':checked') || $('.select').val() == null || $(this).siblings('.form-group').children('input').val().length === 0) {
+      alert('Форма заполнена не верно!');
+    }
+  });
 });
