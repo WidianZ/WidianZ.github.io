66fe8e2e6f95a29fcfbf3adaa6e398b561cb6195
EvEGeNzZz <evegenz@gmail.com>
2017-08-22 21:04:16 +0300

    rosait

--- js/main.js
+++ js/main.js
@@ -1,57 +1,14 @@
-$( document ).ready(function() {
-  
-  $('.modal-link').click(function() {
-    $('.modal-wraper').show();
-    $(document).on('click.modal', function(e) {
-      if (e.target.className == 'modal-wraper' || e.target.className == 'modal_close') {
-        $('.modal-wraper').hide();
-        $(document).off('click.modal');
-      }
-    });
+$(document).ready(function() {
+  $('.carousel').carousel();
+
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    event.preventDefault(); 
+    event.stopPropagation(); 
+    $(this).parent().siblings().removeClass('open');
+    $(this).parent().toggleClass('open');
   });
 
-  $('.select').selectmenu();
-
-  $('.form-control').focus(function() {
-    $(this).parent().addClass('is-active');
-  });
-
-  $('.form-control').blur(function() {
-    if ($(this).val().length === 0) {
-      $(this).parent().removeClass('is-active');
-    }
-  });
-
-  $('#email-field').blur(function() {
-    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-    if ($(this).val().length === 0) {
-
-    } else if (testEmail.test(this.value)) {
-      $(this).parent().addClass('has-success');
-    } else {
-      $(this).parent().addClass('has-error');
-    }
-  });
-
-  $('#name-field').blur(function() {
-    var testName = /^[a-zA-Zа-яА-Я]+$/;
-    if ($(this).val().length === 0) {
-
-    } else if (testName.test(this.value)) {
-      $(this).parent().addClass('has-success');
-    } else {
-      $(this).parent().addClass('has-error');
-    }
-  });
-
-  $('#name-field, #email-field').focus(function() {
-    $(this).parent().removeClass('has-success');
-    $(this).parent().removeClass('has-error');
-  });
-
-  $('.banner-form_accept').click(function() {
-    if ($(this).siblings('.form-group').hasClass('has-error') || !$('#terms-checkbox').is(':checked') || $('.select').val() == null || $(this).siblings('.form-group').children('input').val().length === 0) {
-      alert('Форма заполнена не верно!');
-    }
-  });
-});
+  $('#modalContact').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+})
